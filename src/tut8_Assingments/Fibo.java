8c03e08ec6aaf4a77500ab9702bf54c40d032dd9
KishoreAravindMurugadoss <aravstark4618122001@gmail.com>
2024-01-21 19:58:26 +0530

    Fibo added

---
+++ src/tut8_Assingments/Fibo.java
@@ -0,0 +1,24 @@
+package tut8_Assingments;
+
+import java.util.Scanner;
+
+public class Fibo {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the value of 'n' : ");
+		int n = s.nextInt();
+		int n0 = 0;
+		int n1 = 1;
+		System.out.print(n0 + " , " + n1 + " , ");
+		for (int i = 1; i <= n - 2; i++) {
+			int n2 = n0 + n1;
+			n0 = n1;
+			n1 = n2;
+			System.out.print(n2 + " , ");
+			
+		}
+	}
+
+}
