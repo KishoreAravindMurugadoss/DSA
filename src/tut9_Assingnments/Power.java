f434bbfd63ee419ffa5fa5235fe20c6d228e9115
KishoreAravindMurugadoss <aravstark4618122001@gmail.com>
2024-01-29 19:55:53 +0530

    CommisionCalculator & Power were added

---
+++ src/tut9_Assingnments/Power.java
@@ -0,0 +1,27 @@
+package tut9_Assingnments;
+
+import java.util.Scanner;
+
+public class Power {
+
+	public static void main(String[] args) {
+		System.out.println("---------- Commision Calculator ----------");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter base: ");
+		double base = scanner.nextDouble();
+		System.out.print("to the power of ");
+		int power = scanner.nextInt();
+		double result = powerOf(base, power);
+		System.out.print(base + " to the power of " + power + " is " + result);
+
+	}
+
+	private static double powerOf(double base, int power) {
+		double result = 1;
+		for (int i = 1; i <= power; i++) {
+			result  *= base; 
+		}
+		return result;
+	}
+
+}
