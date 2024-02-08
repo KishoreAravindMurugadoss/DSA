e0c7d25a15f7f3c211ba53be71c9512ef1def411
KishoreAravindMurugadoss <aravstark4618122001@gmail.com>
2024-02-08 20:27:09 +0530

    Biggest_and_Smallest was added

---
+++ src/tut11_Assingments/Biggest_and_Smallest.java
@@ -0,0 +1,46 @@
+package tut11_Assingments;
+
+import java.util.Scanner;
+
+public class Biggest_and_Smallest {
+
+	public static void main(String[] args) {
+		System.out.println("---------- Biggest and Smallest ----------");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("enter:");
+		int a = scanner.nextInt();
+		System.out.print("enter:");
+		int b = scanner.nextInt();
+		System.out.print("enter:");
+		int c = scanner.nextInt();
+		int max = isBig(a, b, c);
+		int min = isSmall(a, b, c);
+		System.out.print("Biggest: " + max);
+		System.out.println();
+		System.out.print("smallest: " + min);
+
+	}
+
+	private static int isSmall(int a, int b, int c) {
+		int min = a;
+		if (b < min) {
+			min = b;
+		}
+		if (c < min) {
+			min = c;
+		}
+		return min;
+	}
+
+	private static int isBig(int a, int b, int c) {
+		int max = a;
+		if (b > max) {
+			max = b;
+		}
+		if (c > max) {
+			max = c;
+		}
+		return max;
+	}
+
+}
